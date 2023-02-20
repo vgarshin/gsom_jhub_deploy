b7e6d43bb1fc937ddd498b2ea73bbf4a02a3f4a0
vgarshin <vgarshin@yandex.ru>
2023-02-20 08:33:19 +0000

    workaround drain script

---
+++ hubdrain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# use in crontab
+# 0 21 * * * /home/simbauser/jhub/hubdrain.sh >> /home/simbauser/jhub/hubdrain.log 2> /home/simbauser/jhub/hubdrainerr.log
+# to drain at 00:00 Moscow time
+# or 0 */12 * * *  - to run every 12 hours
+
+echo $(date) - drain script started
+/usr/bin/kubectl get node -n jhub | while read s; do
+  ws=($s)
+  if [[ ${ws[0]} != NAME ]]; then
+    np=$(/usr/bin/kubectl get pods -o wide -n jhub | grep ${ws[0]} | wc -l)
+    if [[ ${np} > 1 ]]; then
+      echo $(date) - node ${ws[0]} has ${np} podes in jhub namespace
+    else
+      echo $(date) - node ${ws[0]} has no user pods in jhub namespace and will be drained
+      /usr/bin/kubectl drain ${ws[0]} --ignore-daemonsets --delete-emptydir-data --force
+    fi
+  fi
+done
--- migrate_pv_users.sh
+++ migrate_pv_users.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 while read line; do
   echo "* * * * * * * * * * * * * * * * * *"
   echo "Start user ${line} PV copying......"
